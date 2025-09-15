db18657e28c191edae7548e02f931475e96225fa
KushalBadodekar <kbadodekar@hawk.iit.edu>
2025-09-14 19:36:41 -0500

    Changes in Contact Info

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'nuthalapaticharan22@gmail.com';
+  $receiving_email_address = 'kushalbadodekar001@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
